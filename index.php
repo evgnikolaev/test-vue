7e7d99b1f8c5cea18b7fa479bf97844418ecbcb0
Evgeny Nikolaev <e.nikolaev@uplab.ru>
2022-07-13 17:52:04 +0300

    1

--- index.php
+++ index.php
@@ -28,3 +28,83 @@ npm run serve - запуск в режиме разработки
 
 Компонент
 vbase - короткая запись  для компонента
+
+
+
+шаблон компонента:
+			{{}} - интерполяция модели (вывод данных)
+
+		Директивы:
+			v-on:click = @click : - события (@click)
+				@submit.prevent - можно при помощи модификаторов например отменить действие по умолчанию
+			v-for - вывод в цикле
+
+
+1) Двустороннее связывание
+		<input type="text" v-model="post.title">
+		<input type="text" :value="post.title" @input="changeInput">
+
+
+2) Прокидывание данных между компонентами:
+
+		!!! Родитель - ребенок  : через пропсы
+
+				Родитель
+				<post-list :postsP="posts"></post-list>
+
+				Ребенок
+				export default {
+					props: {
+						postsP: {
+							type: Array,
+							required: true
+						}
+					}
+				}
+
+
+
+		!!! Ребенок - родитель  : через события emit
+
+				Родитель
+				<post-form @create="createNewPost"></post-form>
+
+				Ребенок
+				export default {
+					methods: {
+						createPost() {
+							this.$emit('create', this.post);
+							}
+						},
+					}
+
+
+		!!! При помощи v-model - в основном для input, select, checkbox,
+		!!! Также работает и при передаче данных между компонентами
+			Происходит как бы двустороннее связывание : 	Если изменится в родителе, изменится и в ребенке, и наоборот.
+
+				Родитель
+				<my-input v-model="post.title" type="text" placeholder="Название"/>
+
+				Ребенок
+				<input :value="modelValue" @input="updateInput" class="input" type="text">
+
+				export default {
+					name: 'my-input',
+					props: {
+						modelValue: [String, Number]
+					},
+					methods: {
+						updateInput(event) {
+							this.$emit('update:modelValue', event.target.value)
+						}
+					}
+				}
+
+
+
+
+
+
+
+
